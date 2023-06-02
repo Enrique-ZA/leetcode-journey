f116edb12969b431f515a5cd7a7ded7cbc2d368e
Enrique-ZA <60000947+Enrique-ZA@users.noreply.github.com>
2023-06-02 18:54:43 +0200

    Update 49-Group-Anagrams.cpp

--- medium/49-Group-Anagrams.cpp
+++ medium/49-Group-Anagrams.cpp
@@ -1,36 +1,28 @@
-// runtime beats 14% - O(mn) n - size of input array; m - max length of any string in array
-// memory beats 12% - O(mn) n - size of input array; m - max length of any string in array
+// runtime beats 85.18% - O(nmlogm)
+// memory beats 77.64% - O(nm)
 
 class Solution {
 #include <bits/stdc++.h>
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        using namespace std;
-        // 1: hashmap to store array of strings that are anagrams
-        unordered_map<string, vector<string>> res;
-        // 2: loop strs
-        for (auto& s : strs) {
-            // 3: array of integers for a ... z; 26 alphabet letters
-            array<int, 26> count = {0};
-            for (char c : s) {
-                // 4: increment the count for a char
-                count[c - 'a']++;
-            }
-            // 5: key as string for hashmap for each char
-            // seperated by #
-            string key;
-            for (int c : count) {
-                key += '#';
-                key += to_string(c);
-            }
-            //  6: instert string at key into hashmap
-            res[key].push_back(s);
+        // 1: hashmap to store string as key and vector of strings as value
+        std::unordered_map<std::string, std::vector<std::string>> hmap;
+
+        for(std::string& s : strs){
+            // 2: create and string key
+            std::string key = s;
+            std::sort(key.begin(), key.end());
+            
+            // 3: add the string to the vector for the key in the hashmap
+            hmap[key].push_back(s);
         }
-        // 7: return the array of arrays of strings
-        vector<vector<string>> result;
-        for (auto& p : res) {
-            result.push_back(move(p.second));
+
+        // 4: return result
+        std::vector<std::vector<std::string>> result;
+        for(auto& kv : hmap){
+            result.push_back(kv.second);
         }
+        
         return result;
     }
 };
